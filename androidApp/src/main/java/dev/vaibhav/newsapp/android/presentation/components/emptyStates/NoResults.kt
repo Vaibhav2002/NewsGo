afd000761bd442689c4b009c9df0dddcb63a820c
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2022-12-21 21:03:19 +0530

    Designed no results empty state in Android App

---
+++ androidApp/src/main/java/dev/vaibhav/newsapp/android/presentation/components/emptyStates/NoResults.kt
@@ -0,0 +1,57 @@
+package dev.vaibhav.newsapp.android.presentation.components.emptyStates
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.vaibhav.newsapp.android.R
+import dev.vaibhav.newsapp.android.presentation.theme.NewsAppTheme
+
+@Composable
+fun NoResults(
+    modifier:Modifier = Modifier,
+    title:String = "No Articles found",
+    message:String = "Try refreshing to get articles"
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.no_results),
+            contentDescription = "No Results",
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+        )
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun NoResultsPreview() {
+    NewsAppTheme {
+        Surface(){
+            NoResults()
+        }
+
+    }
+}
