ff569241dacb174f75581046aea9d249a61e1f6b
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2022-12-29 19:19:11 +0530

    KeyBoard close on Search Query submitted

--- androidApp/src/main/java/dev/vaibhav/newsapp/android/presentation/screens/search/SearchBox.kt
+++ androidApp/src/main/java/dev/vaibhav/newsapp/android/presentation/screens/search/SearchBox.kt
@@ -2,6 +2,8 @@
 
 package dev.vaibhav.newsapp.android.presentation.screens.search
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Clear
 import androidx.compose.material.icons.rounded.Search
@@ -13,13 +15,16 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 
 @Composable
 fun SearchBox(
-    modifier:Modifier = Modifier,
-    query:String,
-    onQueryChanged:(String)->Unit,
+    modifier: Modifier = Modifier,
+    query: String,
+    onQueryChanged: (String) -> Unit,
 ) {
+    val focusManager = LocalFocusManager.current
     OutlinedTextField(
         modifier = modifier,
         value = query,
@@ -28,6 +33,8 @@ fun SearchBox(
         maxLines = 1,
         textStyle = MaterialTheme.typography.titleMedium,
         label = { Text(text = "Search News") },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(onSearch = { focusManager.clearFocus() }),
         placeholder = { Text(text = "What are you looking for?") },
         trailingIcon = { ClearTextButton { onQueryChanged("") } },
         leadingIcon = { Icon(Icons.Rounded.Search, contentDescription = "Search") }
@@ -36,9 +43,9 @@ fun SearchBox(
 
 @Composable
 private fun ClearTextButton(
-    onClick:()->Unit
+    onClick: () -> Unit
 ) {
-    IconButton(onClick = onClick){
+    IconButton(onClick = onClick) {
         Icon(Icons.Rounded.Clear, contentDescription = "Clear Search Query")
     }
 }
