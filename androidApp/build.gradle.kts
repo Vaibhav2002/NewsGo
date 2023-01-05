56f3e7674147efe0b78068da8d16bde89daf2d3b
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-01-05 18:21:14 +0530

    v1.1.0 APK Added

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1.0"
     }
     buildFeatures {
         compose = true
